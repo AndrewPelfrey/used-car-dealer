1c9b61367be6eed0e43c48b493415ad7c3ef8c6c
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-02-03 22:10:46 -0500

    CarSearch.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav.tsx';
 
+
 function App() {
   return (
     <div>
       <Nav /> {/* Navbar stays on all pages */}
       <main>
         <Outlet /> {/* This will render the correct page based on the route */}
+        
       </main>
     </div>
   );
