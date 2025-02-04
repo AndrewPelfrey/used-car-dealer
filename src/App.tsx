b539c2bd60b916498a2cc857e86c7fe1ede7e22b
CerfSoleil <robedj12@gmail.com>
2025-02-03 19:57:26 -0600

    Added a Header and implemented it across all pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav.tsx';
+import Header from './components/Header.tsx';
 
 function App() {
   return (
     <div>
+      <Header />
       <Nav /> {/* Navbar stays on all pages */}
       <main>
         <Outlet /> {/* This will render the correct page based on the route */}
