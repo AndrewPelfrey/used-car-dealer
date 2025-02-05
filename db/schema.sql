ad19cb0075aa3c25fb5e54db0ae180e9b4ac7aed
laneman952 <kyle.lane1992@yahoo.com>
2025-02-04 20:39:26 -0500

    update order of tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,6 @@ CREATE DATABASE used_car_dealer;
 
 \c used_car_dealer;
 
-CREATE TABLE car (
-    vin VARCHAR(17) PRIMARY KEY,
-    make VARCHAR(50) NOT NULL,
-    model VARCHAR(50) NOT NULL,
-    year INT NOT NULL,
-    mileage INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    trim VARCHAR(50) NOT NULL,
-    car_info_id INT REFERENCES car_info(id)
-);
-
 CREATE TABLE car_info (
     id SERIAL PRIMARY KEY,
     engine VARCHAR(50) NOT NULL,
@@ -24,6 +13,20 @@ CREATE TABLE car_info (
     fuel_eco_city INT NOT NULL,
     fuel_eco_highway INT NOT NULL
 );
+
+CREATE TABLE car (
+    vin VARCHAR(17) PRIMARY KEY,
+    make VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    year INT NOT NULL,
+    mileage INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    trim VARCHAR(50) NOT NULL,
+    interior_color VARCHAR(50) NOT NULL,
+    exterior_color VARCHAR(50) NOT NULL,
+    car_info_id INT REFERENCES car_info(id)
+);
+
 
 CREATE TABLE features (
     id SERIAL PRIMARY KEY,
