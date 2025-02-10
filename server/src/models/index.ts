59dfc5aed670ce58c40db42fad7c90829bc4cf97
laneman952 <kyle.lane1992@yahoo.com>
2025-02-09 22:47:25 -0500

    updated CarFeatures and index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,7 +5,7 @@ import { Sequelize } from 'sequelize';
 import { CarInfoFactory } from './CarInfo';
 import { CarFactory } from './Car';
 import { FeaturesFactory } from './Feature';
-// import { CarFeaturesFactory } from './CarFeature';
+import { CarFeaturesFactory } from './CarFeature';
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
@@ -20,10 +20,11 @@ const sequelize = process.env.DB_URL
 const CarInfo = CarInfoFactory(sequelize);
 const Car = CarFactory(sequelize);
 const Features = FeaturesFactory(sequelize);
-// const CarFeatures = CarFeaturesFactory(sequelize);
+const CarFeatures = CarFeaturesFactory(sequelize);
 
 CarInfo.hasMany(Car, { foreignKey: 'car_info_id' });
 Car.belongsTo(CarInfo, { foreignKey: 'car_info_id' });
-
+Car.belongsToMany(Features, {through: CarFeatures})
+Features.belongsToMany(Car, {through: CarFeatures})
 
 export { sequelize, CarInfo, Car, Features };
