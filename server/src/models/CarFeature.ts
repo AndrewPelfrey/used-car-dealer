ef0b36b438a187f3800a8fedd170254956ad9d0c
Andrew <pelfreya6@gmail.com>
2025-02-10 19:45:17 -0500

    trying to fix render

--- server/src/models/CarFeature.ts
+++ server/src/models/CarFeature.ts
@@ -1,6 +1,37 @@
-// import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
+import { DataTypes, Sequelize, Model } from 'sequelize';
 
-// interface FeaturesAttributes {
-//     id: number;
-//     name: string;
-// }
+interface CarFeaturesAttributes {
+    car_id: string;
+    feature_id: string;
+}
+
+export class CarFeatures extends Model<CarFeaturesAttributes> implements CarFeaturesAttributes {
+   public car_id!: string;
+   public feature_id!: string;
+    
+   // timestamps!
+   public readonly createdAt!: Date;
+   public readonly updatedAt!: Date;
+}
+
+export function CarFeaturesFactory(sequelize: Sequelize) {
+    CarFeatures.init(
+        {
+            car_id: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                references: { model: 'car', key: 'vin' }
+            },
+            feature_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: { model: 'features', key: 'id' }
+            }
+        },
+        {
+            tableName: 'car_features',
+            sequelize,
+        }
+    );
+    return CarFeatures;
+}
