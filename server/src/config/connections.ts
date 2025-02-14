8b34bb8e4e6a6df99df37a4132ddc4fe6b10a082
Andrew <pelfreya6@gmail.com>
2025-02-14 16:56:16 -0500

    media queries

--- server/src/config/connections.ts
+++ server/src/config/connections.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { Sequelize } from 'sequelize';
-
+// COMMENT OUT BELOW TO RUN LOCAL HOST
 const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
       dialect: 'postgres',
@@ -32,6 +32,26 @@ const sequelize = process.env.DATABASE_URL
         },
       }
     );
+// UNCOMMENT THIS, AND COMMENT OUT ABOVE TO RUN ON LOCAL HOST
+    // const sequelize = new Sequelize(
+    //   process.env.DB_NAME as string,
+    //   process.env.DB_USER as string,
+    //   process.env.DB_PASSWORD as string,
+    //   {
+    //     host: process.env.DB_HOST || 'localhost',
+    //     port: parseInt(process.env.DB_PORT as string, 10) || 5432,
+    //     dialect: 'postgres',
+    //     dialectOptions: {
+    //       decimalNumbers: true,
+    //     },
+    //     pool: {
+    //       max: 10,
+    //       min: 0,
+    //       acquire: 30000,
+    //       idle: 10000,
+    //     },
+    //   }
+    // );
 
 
 sequelize.authenticate()
